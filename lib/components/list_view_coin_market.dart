aeb0124b0e312574ced75683b989176c85db0f13
dev <dev@imac-de-m4u-2.m4ucorp.dmc>
2018-03-28 16:08:24 -0300

    navigator implement

--- lib/components/list_view_coin_market.dart
+++ lib/components/list_view_coin_market.dart
@@ -1,3 +1,4 @@
+import 'package:brincando_com_flutter/view/detail/detail_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -9,21 +10,25 @@ class ListViewCoinMarket extends StatefulWidget {
 }
 
 class _ListViewCoinMarketState extends State<ListViewCoinMarket> {
-  
   @override
   Widget build(BuildContext context) {
     return new ListView.builder(
-      itemCount: widget.data.length * 2,
-      itemBuilder: (ctx, i) {
-        if (i.isOdd) return new Divider();
-        int index = i ~/ 2;
-        Map coin = widget.data[index];
-        return  new ListTile(
-            onTap: (){print(coin['name']);},
-            subtitle: new Text("\$ ${coin['price_usd']}"),
-            title: new Text(coin['name']),
-            trailing: new Text("${coin['price_btc']} BTC"));
-      }
-    );
+        itemCount: widget.data.length * 2,
+        itemBuilder: (ctx, i) {
+          if (i.isOdd) return new Divider();
+          int index = i ~/ 2;
+          Map coin = widget.data[index];
+          return new ListTile(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  new MaterialPageRoute(
+                      builder: (context) => new DetailView(coin_name:coin['name'])),
+                );
+              },
+              subtitle: new Text("\$ ${coin['price_usd']}"),
+              title: new Text(coin['name']),
+              trailing: new Text("${coin['price_btc']} BTC"));
+        });
   }
 }
