3e3015410b3943a064e712581ff01d487f2cc961
Bruno Colombini <bruno.colombini@me.com>
2018-03-29 23:22:41 -0300

    montando layout detail view

--- lib/view/detail/detail_view.dart
+++ lib/view/detail/detail_view.dart
@@ -5,20 +5,89 @@ class DetailView extends StatelessWidget {
   DetailView({Key key, this.coin_name}) : super(key: key);
   final String coin_name;
 
+  Column columnConstructor(
+          {List<Widget> widgets,
+          CrossAxisAlignment crossAxisAlignment = CrossAxisAlignment.center}) =>
+      new Column(children: widgets, crossAxisAlignment: crossAxisAlignment);
+  Row rowConstructor({List<Widget> widgets}) => new Row(children: widgets);
+  Container containerConstructor(
+          {Widget child, EdgeInsets padding = const EdgeInsets.all(0.0)}) =>
+      new Container(child: child, padding: padding);
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(this.coin_name),
-      ),
-      body: new Center(
-        child: new RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: new Text('Go back!'),
+        appBar: new AppBar(
+          title: new Text(this.coin_name),
         ),
-      ),
-    );
+        body: containerConstructor(
+            padding: const EdgeInsets.all(32.0),
+            child: columnConstructor(widgets: [
+              rowConstructor(widgets: [
+                new Expanded(
+                  child: columnConstructor(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      widgets: [
+                        containerConstructor(
+                          padding: const EdgeInsets.only(bottom: 8.0),
+                          child: new Text(
+                            'Oeschinen Lake Campground',
+                            style: new TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        new Text(
+                          'Kandersteg, Switzerland',
+                          style: new TextStyle(
+                            color: Colors.grey[500],
+                          ),
+                        ),
+                      ]),
+                ),
+                new Icon(
+                  Icons.star,
+                  color: Colors.red[500],
+                ),
+                new Text('41'),
+              ])
+            ])));
   }
+  //   new Container(
+  // padding: const EdgeInsets.all(32.0),
+  // child: new Column(children: <Widget>[ new Row(
+  //   children: [
+  //     new Expanded(
+  //       child: new Column(
+  //         crossAxisAlignment: CrossAxisAlignment.start,
+  //         children: [
+  //           new Container(
+  //             padding: const EdgeInsets.only(bottom: 8.0),
+  //             child: new Text(
+  //               'Oeschinen Lake Campground',
+  //               style: new TextStyle(
+  //                 fontWeight: FontWeight.bold,
+  //               ),
+  //             ),
+  //           ),
+  //           new Text(
+  //             'Kandersteg, Switzerland',
+  //             style: new TextStyle(
+  //               color: Colors.grey[500],
+  //             ),
+  //           ),
+  //         ],
+  //       ),
+  //     ),
+  //     new Icon(
+  //       Icons.star,
+  //       color: Colors.red[500],
+  //     ),
+  //     new Text('41'),
+  //   ],
+  // ),
+  // ])
+  //   )
+  //   );
+  // }
 }
